ecf51e0b6a65add962a269bed0a973a01de421e7
MaheKarim <mahekarim@gmail.com>
2020-09-27 00:04:30 +0600

    while Loop Add

--- index.php
+++ index.php
@@ -2,14 +2,36 @@
 
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
-            <article id="post-<?php the_ID();?>" <?php post_class(); ?>>
-                <header class="entry-header">
-                    We are Working on Index PHP
-                </header>
-                <div class="entry-content">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet at aut eaque enim eos fugiat in iste magnam minus natus, numquam obcaecati omnis perferendis quam ratione repudiandae sit veniam.</p>
-                </div>
-            </article>
+            
+<!--            --><?php
+//                if (have_posts()) {
+//                	while (have_posts()){
+//                		the_post(); ?>
+<!--		                <!- Content will be here -->
+<!--	              --><?php // }
+//                }
+//            ?>
+	        
+	        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		        <article id="post-<?php the_ID();?>" <?php post_class(); ?>>
+			        <header class="entry-header">
+				        <h3>
+					        <?php the_title(); ?>
+				        </h3>
+			        </header>
+			        <div class="entry-content">
+				        <p>
+					        <?php the_content(); ?>
+				        </p>
+			        </div>
+		        </article>
+	        <?php endwhile; else: ?>
+	            <p>
+		            <?php esc_html_e('No Post Here!', 'mahekarim'); ?>
+	            </p>
+	        <?php endif; ?>
+	        
+            
         </main>
     </div>
 
